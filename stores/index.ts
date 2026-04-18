cdc268be6aca1e1aee80c7fd5f1a42a1d064c249
Rody Huancas <rodyhuancas.04@gmail.com>
2026-04-18 17:46:55 -0500

    feat(stores): add index

---
+++ stores/index.ts
@@ -0,0 +1,4 @@
+export { useTaskStore } from "./task-store";
+export { useDriveStore } from "./drive-store";
+export { useGmailStore } from "./gmail-store";
+export { useThemeStore, type ThemeState, type ThemeMode } from "@/lib/store/theme";
